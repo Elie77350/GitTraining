69f4d7077c4095cf77058a764eb60843f9265892
Elie BINGUE <eliedunamis7@icloud.com>
2024-06-08 14:30:46 +0200

    Initial Commit

---
+++ GitTraining/GitTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTrainingApp.swift
+//  GitTraining
+//
+//  Created by Apprenant 125 on 08/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
